2688c3aff893d132abd32d7db77a3f9161579144
Hesham <heshamelmasry1992@gmail.com>
2023-10-03 19:21:40 +0200

    add configs for prettier eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true
   },
-}
+  extends: [
+    "plugin:react/recommended",
+    "google",
+    "plugin:prettier/recommended"
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "require-jsdoc": "off",
+    "react/react-in-jsx-scope": "off",
+    "new-cap": "off"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {Provider} from "react-redux"; // This makes the store global, So i can access it anywhere I want.
-import store from './store'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux"; // This makes the store global, So i can access it anywhere I want.
+import store from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-)
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);

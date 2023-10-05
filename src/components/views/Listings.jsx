fc5e6eafebedd3abf9844b2a4c8dd92f3118d109
Hesham <heshamelmasry1992@gmail.com>
2023-10-05 18:36:15 +0200

    add placeholder image - create a separate file for the data - function to handle if no image - handle if not in stock - create a fake API call

--- src/components/views/Listings.jsx
+++ src/components/views/Listings.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchProducts } from "../../store/modules/listingsSlice.js";
+import productImgPlaceholder from "../../assets/images/no-image-placeholder.svg";
 
 function Listings() {
   const dispatch = useDispatch(); // Help to dispatch actions, Example: dispatch(fetchProducts())
@@ -10,7 +12,26 @@ function Listings() {
     dispatch(fetchProducts());
   }, [dispatch]);
 
-  console.log(products);
+  function getProductImage(img) {
+    if (img && img.imageSrc) {
+      return (
+        <img
+          src={img.imageSrc}
+          alt={img.imageAlt || "Product Image"}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
+      );
+    } else {
+      return (
+        <img
+          src={productImgPlaceholder}
+          alt="No product image available"
+          className="h-full w-full object-contain object-center group-hover:opacity-75"
+        />
+      );
+    }
+  }
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -18,19 +39,25 @@ function Listings() {
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
+            <Link
+              to={`listing/${product.id}`}
+              key={product.id}
+              className="group relative"
+            >
+              {!product.available && (
+                <span className="absolute top-0 h-8 bg-red-500 w-fit z-10 rounded-tr-lg right-0 px-4 flex items-center justify-center text-white font-bold uppercase text-xs">
+                  out of stock
+                </span>
+              )}
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-100 hover:bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 border border-y-gray-300">
+                {getProductImage(product)}
               </div>
               <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+              <h4 className="mt-4 text-xs text-gray-700">{product.brand}</h4>
               <p className="mt-1 text-lg font-medium text-gray-700">
-                {product.price}
+                NOK {product.price}
               </p>
-            </a>
+            </Link>
           ))}
         </div>
       </div>
